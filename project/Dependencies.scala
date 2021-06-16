14db599cf4bb3b09cd541af6baa41ae320cbc4fa
eltherion <mateusz.murawski@datart.pl>
2021-06-16 19:06:06 +0200

    Initial commit.

---
+++ project/Dependencies.scala
@@ -0,0 +1,27 @@
+import sbt._
+
+object Dependencies {
+  private val betterFiles     = "com.github.pathikrit"        %% "better-files"                         % VersionsOf.betterFiles
+  private val fs2Core         = "co.fs2"                      %% "fs2-core"                             % VersionsOf.fs2
+  private val fs2IO           = "co.fs2"                      %% "fs2-io"                               % VersionsOf.fs2
+  private val http4sCore      = "org.http4s"                  %% "http4s-blaze-server"                  % VersionsOf.http4s
+  private val http4sDsl       = "org.http4s"                  %% "http4s-dsl"                           % VersionsOf.http4s
+  private val http4sServer    = "org.http4s"                  %% "http4s-blaze-server"                  % VersionsOf.http4s
+  private val janino          = "org.codehaus.janino"         %  "janino"                               % VersionsOf.janino
+  private val logbackClassic  = "ch.qos.logback"              %  "logback-classic"                      % VersionsOf.logbackClassic
+  private val scalaLogging    = "com.typesafe.scala-logging"  %% "scala-logging"                        % VersionsOf.scalaLogging
+  private val scalatest       = "org.scalatest"               %% "scalatest"                            % VersionsOf.scalatest      % Test
+
+  val all: Seq[ModuleID] = Seq(
+    betterFiles,
+    fs2Core,
+    fs2IO,
+    janino,
+    http4sCore,
+    http4sDsl,
+    http4sServer,
+    logbackClassic,
+    scalaLogging,
+    scalatest
+  )
+}
---
+++ project/VersionsOf.scala
@@ -0,0 +1,9 @@
+object VersionsOf {
+  val betterFiles     = "3.9.1"
+  val fs2             = "3.0.4"
+  val http4s          = "1.0.0-M23"
+  val janino          = "3.1.4"
+  val logbackClassic  = "1.2.3"
+  val scalaLogging    = "3.9.3"
+  val scalatest       = "3.2.9"
+}
